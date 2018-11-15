6d134f1b79b5c6558420e01bdd6a9ddca60d0087
ethansha <136856026@qq.com>
2018-11-15 11:42:02 +0800

    first build

---
+++ src/main/java/com/citybrain/agenthub/AgenthubApplication.java
@@ -0,0 +1,12 @@
+package com.citybrain.agenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenthubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgenthubApplication.class, args);
+    }
+}
